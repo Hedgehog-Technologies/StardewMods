ddc5f3466ff4e43dd836cb0c94918d3bb9e551aa
Jag <jag3dagster@gmail.com>
2024-04-21 22:13:54 -0700

    feat(fullfishingbar): Add Full Fishing Bar mod
    - Makes your fishing bobber bar take up the full fishing bobber track

---
+++ src/FullFishingBar/ModEntry.cs
@@ -0,0 +1,43 @@
+﻿using FullFishingBar;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley.Menus;
+
+namespace AutoForager
+{
+	/// <summary>
+	/// The mod entry point.
+	/// </summary>
+	public class ModEntry : Mod
+	{
+		private ModConfig _config = new();
+
+		public override void Entry(IModHelper helper)
+		{
+			_config = helper.ReadConfig<ModConfig>();
+
+			helper.Events.Display.MenuChanged += OnMenuChanged;
+			helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+		}
+
+		private void OnMenuChanged(object? sender, MenuChangedEventArgs e)
+		{
+			if (!_config.IsEnabled) return;
+
+			if (e.NewMenu is BobberBar)
+			{
+				var bb = e.NewMenu as BobberBar;
+
+				if (bb is not null)
+				{
+					bb.bobberBarHeight = BobberBar.bobberBarTrackHeight;
+				}
+			}
+		}
+
+		private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+		{
+			_config.RegisterModConfigMenu(Helper, ModManifest);
+		}
+	}
+}
