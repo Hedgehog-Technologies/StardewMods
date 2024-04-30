e84811572d91b375c17ac64ad290f12a88a02268
Jag <jag3dagster@gmail.com>
2024-04-29 21:20:43 -0700

    Initial check-in

---
+++ src/AutoTrasher/ModEntry.cs
@@ -0,0 +1,15 @@
+﻿using StardewModdingAPI;
+
+namespace AutoTrasher
+{
+	/// <summary>
+	/// The mod entry point.
+	/// </summary>
+	public class ModEntry : Mod
+	{
+		public override void Entry(IModHelper helper)
+		{
+
+		}
+	}
+}
