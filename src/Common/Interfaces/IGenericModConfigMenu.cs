6df59609814ff53a9dc1840ae1281bf4f6d514a1
Jag <77469789+jag3dagster@users.noreply.github.com>
2026-08-15 13:32:49 -0700

    Downgrade GMCM api to 1.15

--- src/Common/Interfaces/IGenericModConfigMenu.cs
+++ src/Common/Interfaces/IGenericModConfigMenu.cs
@@ -36,7 +36,6 @@ namespace HedgeTech.Common.Interfaces
 		/// <param name="tooltip">The tooltip text shown when the cursor hovers on the title, or <c>null</c> to disable the tooltip.</param>
 		void AddSectionTitle(IManifest mod, Func<string> text, Func<string> tooltip = null);
 
-
 		/// <summary>Add a paragraph of text at the current position in the form.</summary>
 		/// <param name="mod">The mod's manifest.</param>
 		/// <param name="text">The paragraph text to display.</param>
@@ -95,7 +94,7 @@ namespace HedgeTech.Common.Interfaces
 		/// <param name="fieldId">The unique field ID for use with <see cref="OnFieldChanged"/>, or <c>null</c> to auto-generate a randomized ID.</param>
 		void AddTextOption(IManifest mod, Func<string> getValue, Action<string> setValue, Func<string> name, Func<string> tooltip = null, string[] allowedValues = null, Func<string, string> formatAllowedValue = null, string fieldId = null);
 
-		/// <summary>Add a key binding at the current position in the form.</summary>
+		/// <summary>Add a keybind at the current position in the form.</summary>
 		/// <param name="mod">The mod's manifest.</param>
 		/// <param name="getValue">Get the current value from the mod config.</param>
 		/// <param name="setValue">Set a new value in the mod config.</param>
@@ -104,7 +103,7 @@ namespace HedgeTech.Common.Interfaces
 		/// <param name="fieldId">The unique field ID for use with <see cref="OnFieldChanged"/>, or <c>null</c> to auto-generate a randomized ID.</param>
 		void AddKeybind(IManifest mod, Func<SButton> getValue, Action<SButton> setValue, Func<string> name, Func<string> tooltip = null, string fieldId = null);
 
-		/// <summary>Add a key binding list at the current position in the form.</summary>
+		/// <summary>Add a keybind list at the current position in the form.</summary>
 		/// <param name="mod">The mod's manifest.</param>
 		/// <param name="getValue">Get the current value from the mod config.</param>
 		/// <param name="setValue">Set a new value in the mod config.</param>
@@ -166,6 +165,10 @@ namespace HedgeTech.Common.Interfaces
 		/// <summary>Open the config UI for a specific mod.</summary>
 		/// <param name="mod">The mod's manifest.</param>
 		void OpenModMenu(IManifest mod);
+
+		/// <summary>Open the config UI for a specific mod, as a child menu if there is an existing menu.</summary>
+		/// <param name="mod">The mod's manifest.</param>
+		void OpenModMenuAsChildMenu(IManifest mod);
 
 		/// <summary>Get the currently-displayed mod config menu, if any.</summary>
 		/// <param name="mod">The manifest of the mod whose config menu is being shown, or <c>null</c> if not applicable.</param>
